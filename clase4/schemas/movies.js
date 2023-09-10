80f14d332e55736db4405571d9d7b7dbe6685062
JuanPabloRP <jpabloroldanp@gmail.com>
2023-09-09 19:16:22 -0500

    feat: clase4

---
+++ clase4/schemas/movies.js
@@ -0,0 +1,36 @@
+import z from 'zod'
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title es required'
+  }),
+  genre: z.array(
+    z.enum([
+      'Action',
+      'Adventure',
+      'Comedy',
+      'Drama',
+      'Fantasy',
+      'Horror',
+      'Thriller',
+      'Sci-Fi',
+      'Crime'
+    ])
+  ),
+  year: z.number().int().min(1900).max(2024),
+  duration: z.number().int().positive(),
+  director: z.string(),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL'
+  }),
+  rate: z.number().min(0).max(10).default(5)
+})
+
+export function validateSchema (object) {
+  return movieSchema.safeParse(object)
+}
+
+export function validatePartialSchema (object) {
+  return movieSchema.partial().safeParse(object)
+}
